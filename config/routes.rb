91b86bf1b0df1e29de1cefccc37823d63d2f087d
Benjamin Topping <bt8@mib113631i.internal.sanger.ac.uk>
2019-11-04 10:25:57 +0000

    Friends search
    Added friends search with error partial

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :registrations => "user/registrations"}
   resources :user_stocks, only:[:create, :destroy]
+  resources :users, only:[:show]
+  resources :friendships
 
   root 'welcome#index'
   get 'my_portfolio', to:'users#my_portfolio'
   get 'search_stocks', to:'stocks#search'
   get 'my_friends', to:'users#my_friends'
+  get 'search_friends', to:'users#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
